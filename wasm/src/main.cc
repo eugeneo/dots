7e06593dd1619913fa355ec8e23cc042f09d81e4
Eugene Ostroukhov <eostroukhov@gmail.com>
2025-09-16 09:50:33 -0700

    Experimenting

--- wasm/src/main.cc
+++ wasm/src/main.cc
@@ -1,17 +1,23 @@
-#include <emscripten/bind.h>
-
 #include <iostream>
 #include <string>
 
+#include <emscripten/bind.h>
+
+#include "absl/strings/str_cat.h"
+
+namespace test {
+
+int add(int a, int b);
+std::string simd();
+
+} // namespace test
+
 std::string concat_strings(const std::string &a, const std::string &b) {
-  return a + b;
+  return absl::StrCat(a, " ", test::add(2, 3), " ", b, " ", test::simd());
 }
 
 EMSCRIPTEN_BINDINGS(my_module) {
   emscripten::function("concat_strings", &concat_strings);
 }
 
-int main() {
-  std::cout << "Hello, WebAssembly!" << std::endl;
-  return 0;
-}
+int main() { return 0; }
